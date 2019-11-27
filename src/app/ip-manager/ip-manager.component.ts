6c34e10d2100d82a49d7aaa18db99e5d067a67f4
Raksha <raksha.kotian@novaders.com>
2019-11-27 21:40:33 +0530

    minus icon removed for first element and popup msg for limit exceed

--- src/app/ip-manager/ip-manager.component.ts
+++ src/app/ip-manager/ip-manager.component.ts
@@ -6,7 +6,7 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ip-manager',
@@ -24,61 +24,46 @@ export class IpManagerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.currentPlan=localStorage.getItem('usertype');
+    this.currentPlan = localStorage.getItem('usertype');
     this.ipformInit();
     this.updateExistingIp();
     this.getTypeFromStorage = localStorage.getItem('usertype');
     if (this.getTypeFromStorage != null) {
-     this.currenttype = this.getTypeFromStorage;
+      this.currenttype = this.getTypeFromStorage;
     }
   }
- // Handle Add button permission based on user Plan
- enableAddbtn() {
 
-  const ipCollectionlen = this.ipCollection.length;
-  if (this.currenttype != null) {
-      if (this.currenttype === 'Premium') {
-        if (ipCollectionlen >= 10) {
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        if (ipCollectionlen >= 5) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+  showMsg() {
+    Swal.fire('Warning!',
+      'Cannot add more items',
+      'warning');
   }
-}
-
-// Populate UI elements if already present in localstorage
-updateExistingIp() {
-  if (localStorage.getItem('ipCollection') !== null) {
-    const data = JSON.parse(localStorage.getItem('ipCollection'));
-    this.ipCollection = data;
+  // Populate UI elements if already present in localstorage
+  updateExistingIp() {
+    if (localStorage.getItem('ipCollection') !== null) {
+      const data = JSON.parse(localStorage.getItem('ipCollection'));
+      this.ipCollection = data;
+    }
   }
-}
 
 
- // To initialise and reset IP address
-ipformInit() {
-  this.ipCollection = [{
-    ip : '',
-    valid: false
-  }];
-}
+  // To initialise and reset IP address
+  ipformInit() {
+    this.ipCollection = [{
+      ip: '',
+      valid: false
+    }];
+  }
 
 
   // Saves IP address collection
   onSaveIPCollection() {
     const ipCollectionStr = JSON.stringify(this.ipCollection);
-    localStorage.setItem('ipCollection', ipCollectionStr );
-    Swal.fire( 'Success!',
-    'Successfully added IP Address',
-    'success');
-    }
+    localStorage.setItem('ipCollection', ipCollectionStr);
+    Swal.fire('Success!',
+      'Successfully added IP Address',
+      'success');
+  }
 
   // Reset a user Plan
   removeplan(): void {
@@ -95,38 +80,64 @@ ipformInit() {
 
   // IP address validator
   isValidIP(item) {
-      const ip = item.ip;
-      if (ip) {
-        const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
-        const regex = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
-        item.valid = regex.test(ip);
-        return regex.test(ip);
-      } else {
-        item.valid = false;
-        return true;
-      }
+    const ip = item.ip;
+    if (ip) {
+      const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
+      const regex = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
+      item.valid = regex.test(ip);
+      return regex.test(ip);
+    } else {
+      item.valid = false;
+      return true;
+    }
   }
 
   // Save the IP collection if no validation error
-   isSaveValid() {
-    const errfiltered = this.ipCollection.filter( v => v.valid === false );
-    const atleastoneIP = this.ipCollection.filter( v => v.ip !== '' );
+  isSaveValid() {
+    const errfiltered = this.ipCollection.filter(v => v.valid === false);
+    const atleastoneIP = this.ipCollection.filter(v => v.ip !== '');
     if (errfiltered.length == 0 && atleastoneIP.length > 0) {
       return false;
     } else {
       return true;
     }
-   }
-
-  // Insert a new UI element to add IP address
-  addIpAddr(val) {
-    this.ipCollection.push({ip: '', valid: false});
   }
 
+  /**
+   * @description Handle Add button permission based on user Plan,
+   * Insert a new UI element to add IP address
+   *  */
+  addIpAddr(val) {
+    const ipCollectionlen = this.ipCollection.length;
+    if (this.currenttype != null) {
+      if (this.currenttype === 'Premium') {
+        if (ipCollectionlen >= 10) {
+          this.showMsg();
+        } else {
+          this.ipCollection.push({ ip: '', valid: false });
+        }
+      } else {
+        if (ipCollectionlen >= 5) {
+          this.showMsg();
+        } else {
+          this.ipCollection.push({ ip: '', valid: false });
+        }
+      }
+    }
+
+  }
+  //check collection length and show/hide minus icon
+  checkCollection(isFirst) {
+    if (isFirst) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   // Remove selected UI text box and clear input box if its the last element
   deleteIpaddr(index) {
     if (this.ipCollection.length === 1) {
-     this.ipformInit();
+      this.ipformInit();
     } else {
       this.ipCollection.splice(index, 1);
     }
